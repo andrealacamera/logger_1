93ca3fcfb8d0b13f575a4e374c884dc6d9e39a32
andrealacamera <andrea.lacamera0@gmail.com>
2021-10-13 12:40:05 +0200

    some improvements...

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const logger = require('./logger');
 
-// console.log('log');
-// console.warn('warn');
+//instead of the standard console.log() ...
+// console.log('some information');
+// console.warn('warning');
 // console.error('error');
 
-logger.info(process.env.NODE_ENV)
-logger.log('info', "TEST")
-logger.error("AHIA!!!")
+//... use logger!
+logger.info(`NODE_ENV = ${process.env.NODE_ENV}`);
+logger.warn(`a simple warning for you!`, {meta: 111});
+logger.error("something's wrong here");
+logger.debug("debug info");
+logger.log('debug', "same of the previous line");
+
+logger.error(new Error(`error message!`))
--- logger/index.js
+++ logger/index.js
@@ -1,14 +1,15 @@
 const { createLogger, format, transports } = require('winston');
-const { combine, timestamp, label, printf } = format;
+const { combine, timestamp, printf } = format;
 
-const theFormat = printf(({ level, message, timestamp }) => {
-  return `${timestamp} -> [${level}]: ${message}`;
+const theFormat = printf(({ level, message, timestamp, stack }) => {
+  return `${timestamp} -> [${level}]: ${stack || message}`;
 });
 const logger = createLogger({
   level: 'debug',
   format: format.combine(
     format.timestamp(),
     format.colorize(),
+    format.errors({stack: true}),
     theFormat
   ),
   transports: [new transports.Console()]
